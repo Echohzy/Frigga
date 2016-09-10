ea944bcc7f20625d7343ed18f1ed50bebaffae94
Echohzy <1820123540@qq.com>
2016-09-10 22:19:53 +0800

    add footer component

---
+++ App/javascripts/components/footer_component.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome.js';
+
+import {baseColor} from "../../stylesheets/base.js";
+
+export default class FooterComponent extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <View style={styles.footerContainer}>
+        <TouchableOpacity style={styles.footerItem} onPress={()=>this.props.changeView("home")}>
+          <View>
+            <Icon style={styles.footerItemIcon} name="home" size={24}/>
+            <Text style={styles.footerItemText}>首页</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerItem} onPress={()=>this.props.changeView("editor")}>
+          <View>
+            <Icon style={styles.footerItemIcon} name="plus" size={24}/>
+            <Text style={styles.footerItemText}>新建</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerItem} onPress={()=>this.props.changeView("user")}>
+          <View>
+            <Icon style={styles.footerItemIcon} name="user" size={24}/>
+            <Text style={styles.footerItemText}>用户</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+};
+
+var styles = StyleSheet.create({
+  footerContainer: {
+    flexWrap: "nowrap",
+    flexDirection: "row",
+    height: 60
+  },
+  footerItem: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  footerItemIcon: {
+    color: baseColor.textGrey
+  },
+  footerItemText: {
+    fontSize: 12,
+    marginTop: 4,
+    color: baseColor.textGrey
+  }
+});
--- App/javascripts/pages/home.js
+++ App/javascripts/pages/home.js
@@ -6,15 +6,24 @@ import {
 } from "react-native";
 
 import ArticleListContainer from "../containers/article_list_container";
+import FooterComponent from "../components/footer_component";
 
 export default class Home extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      view: "home"
+    };
+    this.changeView = this.changeView.bind(this);
+  }
+  changeView(item){
+    this.setState({view: item});
   }
   render(){
     return (
       <View style={{flex: 1}}>
         <ArticleListContainer navigator={this.props.navigator}/>
+        <FooterComponent changeView={this.changeView} selectedItem={this.state.view} />
       </View>
     );
   }
